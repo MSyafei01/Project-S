ee5b2c958e15c886c18c86dc02f2f8534898736b
MSyafei <mochamadsyafei2701@gmail.com>
2025-09-25 09:24:42 +0700

    up

--- js/index.js
+++ js/index.js
@@ -3,17 +3,17 @@ const products = [
   {
     name: "Bouquet Mawar Merah",
     price: "Rp 250.000",
-    image: "https://github.com/MSyafei01/Project-S/blob/master/mawarmerah.jpg"
+    image: "mawarmerah.jpg"
   },
   {
     name: "Bouquet Tulip",
     price: "Rp 350.000",
-    image: "https://github.com/MSyafei01/Project-S/blob/master/mawarmerah.jpg"
+    image: "mawarmerah.jpg"
   },
   {
     name: "Bouquet Lily",
     price: "Rp 300.000",
-    image: "https://github.com/MSyafei01/Project-S/blob/master/mawarmerah.jpg"
+    image: "mawarmerah.jpg"
   }
 ];
 
